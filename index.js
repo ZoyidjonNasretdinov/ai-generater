f64fc6d2e33b9dd90d43afcc6b6391bacd2e85e9
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-03-10 17:26:56 +0500

    generate grid gallery

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const themeToggle = document.querySelector('.theme-toggle');
 const promptBtn = document.querySelector('.prompt-btn');
 const promptInput = document.querySelector('.prompt-input');
+const promptForm = document.querySelector('.prompt-form');
+const modelSelect = document.querySelector('.model-select');
+const countSelect = document.querySelector('.count-select');
+const ratioSelect = document.querySelector('.ratio-select');
+const gridGallery = document.querySelector('.grid-gallery');
 
 
 const examplePrompt = [
@@ -30,9 +35,41 @@ themeToggle.addEventListener('click', () => {
 });
 
 
+// Create image cards
+const createImageCards = async (selectedModel, imageCount, aspectRatio, promptText) => {
+  gridGallery.innerHTML = "";
+  for (let i = 0; i < imageCount; i++) {
+    gridGallery.innerHTML += `
+              <div class="img-card loading" id="img-card-${i}" style="--aspect-ratio: ${aspectRatio}">
+                <div class="status-container">
+                  <div class="spinner"></div>
+                  <i class="fa-solid fa-triangle-exclamation"></i>
+                  <p class="status-text">Generating...</p>
+                </div>
+                <img src="./images/image.png" alt="Generated Image" class="result-img">
+             </div>
+    
+    `
+  }
+}
+
+const handleFormSubmit = (e) => {
+  e.preventDefault();
+  const selectedModel = modelSelect.value;
+  const imageCount = parseInt(countSelect.value) || 1;
+  const aspectRadio = ratioSelect.value || "1/1";
+  const promptText = promptInput.value.trim();
+
+  createImageCards(selectedModel, imageCount, aspectRadio, promptText);
+}
+
+
 // Generate random prompt
 promptBtn.addEventListener('click', () => {
   const prompt = examplePrompt[Math.floor(Math.random() * examplePrompt.length)];
   promptInput.value = prompt;
   promptInput.focus();
 });
+
+promptForm.addEventListener('submit', handleFormSubmit);
+
